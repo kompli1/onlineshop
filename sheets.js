d3bd27831e358356c102bc9c2a3c4f69b3c9f5e3
kompli1 <kokokompi@proton.me>
2026-01-30 16:12:09 +0500

    Update SHEETS_WEBHOOK to a valid URL

--- sheets.js
+++ sheets.js
@@ -1,4 +1,4 @@
-const SHEETS_WEBHOOK = "ВСТАВЬ_URL_СЮДА";
+const SHEETS_WEBHOOK = "https://script.google.com/macros/s/AKfycbyXX-AQuwsz8i0owIwBn3GfFeFJE-F5J0aYkq2EWIeQTaMDXNX4WHXnK6vVbjzCaZeL/exec";
 
 async function sendOrderToSheets(data) {
   await fetch(SHEETS_WEBHOOK, {
