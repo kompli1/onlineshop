0c60f19c4fa98fdaebeee75723f70d3522845ad3
kompli1 <kokokompi@proton.me>
2026-01-30 16:47:41 +0500

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -1,11 +1,10 @@
 const SHEETS_WEBHOOK = "https://script.google.com/macros/s/AKfycbykfJON5DSDXcgAedxk1OOcDtDliouNBwFfYQedQVqKDDJC3LGuZs5BW1D7DTlNUK3g/exec";
 
 async function sendOrderToSheets(data) {
-  await fetch(SHEETS_WEBHOOK, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  });
+await fetch(SHEETS_WEBHOOK, {
+  method: "POST",
+  mode: "no-cors",
+  body: JSON.stringify(data)
+});
+
 }
