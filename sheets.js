a9574032a86f8da50f8e92ce11df15bb78f9de24
kompli1 <kokokompi@proton.me>
2026-01-30 16:55:03 +0500

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -1,10 +1,17 @@
-const SHEETS_WEBHOOK = "https://script.google.com/macros/s/AKfycbykfJON5DSDXcgAedxk1OOcDtDliouNBwFfYQedQVqKDDJC3LGuZs5BW1D7DTlNUK3g/exec";
+const SHEETS_WEBHOOK = "https://script.google.com/macros/s/AKfycbyXX-AQuwsz8i0owIwBn3GfFeFJE-F5J0aYkq2EWIeQTaMDXNX4WHXnK6vVbjzCaZeL/exec";
 
-async function sendOrderToSheets(data) {
-await fetch(SHEETS_WEBHOOK, {
-  method: "POST",
-  mode: "no-cors",
-  body: JSON.stringify(data)
-});
+function sendOrderToSheets(data) {
+  const form = document.createElement("form");
+  form.method = "POST";
+  form.action = SHEETS_WEBHOOK;
+  form.target = "hidden_iframe";
 
+  const input = document.createElement("input");
+  input.name = "payload";
+  input.value = JSON.stringify(data);
+
+  form.appendChild(input);
+  document.body.appendChild(form);
+  form.submit();
+  form.remove();
 }
